01337639b736e59f3d1c19341625975a825ebec3
Atharv Desai <atharvdesai2002@gmail.com>
2022-04-28 18:19:03 +0530

    Add files via upload

---
+++ Data/Migrations/20220428084141_initalsetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VMS_v1._2.Data.Migrations
+{
+    public partial class initalsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VMS_v1._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
